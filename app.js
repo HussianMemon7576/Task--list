db3c9340c128374bb80ec07138264592ded52e04
HussianMemon7576 <memonhussain753@gmail.com>
2025-12-12 20:00:05 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+ const ul = document.getElementById("tasks-list");
+ const button = document.getElementsByClassName("red");
+ const input = document.getElementsById("text-feild");
+
+ function addTask(){
+   const input = tasks
+ }
+
